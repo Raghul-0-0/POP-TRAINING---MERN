28ed3da1a761b0708bb03c2c6dcd2918b33e72b6
Raghul <raghulvishva1016@gmail.com>
2025-02-14 13:25:32 +0530

    day 6 FN

---
+++ day6/print.js
@@ -0,0 +1,33 @@
+console.log("Hello World");
+
+// Declaring variables 
+a = 10;
+b = "abc";
+c = true;
+console.log(a,b,c);
+// primmitive or primary Datatypes
+var a = 10.54334
+let b = "abc"
+const c = true
+console.log(a,b,c);
+
+console.log(typeof(a), typeof(b), typeof(c));
+array=[1,6,"abc",true];
+console.log(array);
+
+object1={
+    "firstname":"Joseph",
+    "lastname":"institiute",
+    "depts":["ads","cse","ece"],
+    "training": "MERN"
+}
+
+console.log(object1);
+
+//types of object declaration
+obj2 = new Object();
+obj2["name"]="kessi";
+obj2["nuh"]="uh";
+console.log(obj2.name)
+console.log(obj2["nuh"]);
+

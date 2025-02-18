c2f1fdaf3f457635b49c09c7b07863ae72c18877
Raghul <raghulvishva1016@gmail.com>
2025-02-18 14:13:13 +0530

    day 8 FN

--- day7/advanced_js/async_await.js
+++ day7/advanced_js/async_await.js
@@ -1,7 +1,39 @@
-async function asAw() {
+// async function asAw(){
+//     console.log("Hello");
+// }
 
-    setTimeout(function(){
-        console.log("HELLO");},4000)
+// async function asAw(){
+//     var a = 15
+//     setTimeout(function(){
+//         console.log("hello");
+//     }, 5000)
+//     console.log(await a);
+// }
+// console.log(asAw());
+// asAw().then((res) =>{
+//     console.log(res);
+// }).catch((res) => {
+//     console.log("res");
+// })
+
+//console.log(await asAw());
+
+//Instagram console Demo for post,like,comment,share
+
+likeCode = ()=>{
+    return new Promise((likePost) => {
+        setTimeout(() => {
+            createPost("Post liked succesfully")
+        })
+    }
+)
 }
 
-console.log(asAw());
+async function postCode(){
+    var post = new Promise((createPost, reject) =>{
+        createPost("post created successfulyll!")
+    })
+    console.log(await post)
+}
+
+postCode()
